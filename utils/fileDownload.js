ef25865be3d86b7b455f6a69404d00d710acce9e
jackrosemrcglobal <jack.rose@mrcglobal.com>
2025-06-20 13:48:56 -0400

    Delete utils directory

--- utils/fileDownload.js
+++
@@ -1,9 +0,0 @@
-export const downloadFile = (content, fileName, contentType) => {
-    const a = document.createElement("a");
-    const file = new Blob([content], { type: contentType });
-    a.href = URL.createObjectURL(file);
-    a.download = fileName;
-    a.click();
-    URL.revokeObjectURL(a.href);
-};
-
